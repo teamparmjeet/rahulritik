a07feab76136a8ca107083ae0ee6ae1f8e919ded
teamparmjeet <teamparmjeet@gmail.com>
2025-05-14 12:20:11 +0530

    new

--- components/updatequery/updatequery/Updatequery.jsx
+++ components/updatequery/updatequery/Updatequery.jsx
@@ -194,6 +194,7 @@ export default function UpdateQuery({ query, audit }) {
             className="block w-full p-3 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-[#29234b] focus:border-[#29234b]"
           >
             <option value="" disabled>-- Select Interested Status --</option>
+            
             <option value="online">Online</option>
             <option value="ofline">Offline</option>
             <option value="response">Response</option>
